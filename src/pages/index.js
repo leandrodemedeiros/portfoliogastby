bc81bb105b13f6db42bfb1f4536a7817206dbac0
leandrodemedeiros <77240253+leandrodemedeiros@users.noreply.github.com>
2022-10-30 03:54:58 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,14 +13,14 @@ import {
 export default function IndexPage() {
   return (
     <>
-      <Seo title="Gatsby Starter for Portfolio Minimal" />
+      <Seo title="Leandro de Medeiros — Portfolio" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
-        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
-        <AboutSection sectionId="about" heading="About Portfolio Minimal" />
-        <InterestsSection sectionId="details" heading="Details" />
-        <ProjectsSection sectionId="features" heading="Built-in Features" />
-        <ContactSection sectionId="github" heading="Issues?" />
+        <ArticlesSection sectionId="articles" heading="Artigos recentes" sources={['Medium']} />
+        <AboutSection sectionId="about" heading="Sobre mim" />
+        <InterestsSection sectionId="details" heading="Habilidades e interesses" />
+        <ProjectsSection sectionId="features" heading="Serviços" />
+        <ContactSection sectionId="github" heading="Quer falar comigo?" />
       </Page>
     </>
   );
